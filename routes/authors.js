ae102219b14dfb9c88a18659d19beecd614c8dcb
mrkstechs <kelechi.okoli@hotmail.co.uk>
2024-07-09 01:06:09 +0100

    finished author routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Author = require('../models/author')
+const Book = require('../models/book')
 const router = express.Router()
 
 // All Authors Route
@@ -30,13 +31,66 @@ router.post('/', async (req, res) => {
 
     try {
         const newAuthor = await author.save()
-        // res.redirect(`authors/${newAuthor.id}`)
-        res.redirect(`authors`)
+        res.redirect(`authors/${newAuthor.id}`)
     } catch (error) {
         res.render('authors/new', {
             author: author,
             errorMessage: 'Error creating Author'
         })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        const books = await Book.find({author: author.id}).limit(6).exec()
+        res.render('authors/show', { author, booksByAuthor: books})
+    } catch (error) {
+        res.redirect('/')   
+    }
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit', { author: author } )
+    } catch (error) {
+        res.redirect('/authors')
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    let author 
+    try {
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()
+        res.redirect(`/authors/${author.id}`)
+    } catch (error) {
+        if (author == null){
+            res.redirect('/')
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error updating Author'
+            })
+        }
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    let author 
+    try {
+        author = await Author.findById(req.params.id)
+        await author.deleteOne()
+        res.redirect(`/authors`)
+    } catch (error) {
+        if (author == null){
+            res.redirect('/')
+        } else {
+            console.log(error)
+            res.redirect(`/authors/${author.id}`)
+        }
     }
 })
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ if(process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
 
 const app = express()
 
@@ -15,6 +16,7 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(express.urlencoded({limit: '10mb', extended: false}))
 
